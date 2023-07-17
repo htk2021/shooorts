82c01e9e2b126437f996fdc95243918b8e976f2e
parkyounbae <yache1217@naver.com>
2023-07-17 16:24:17 +0900

    add apis

--- lib/UsingApi.dart
+++ lib/UsingApi.dart
@@ -1,8 +1,11 @@
+import 'package:flutter2/models/Comment.dart';
+import 'package:flutter2/models/Shorts.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class ApiClient {
   static const url = 'http://172.10.5.167:80/';
+
   Future<void> addUser(String name, String userId) async {
     const url_withfunc=url+'user/add';
     final response = await http.post(
@@ -22,8 +25,111 @@ class ApiClient {
     }
   }
 
+  // Shorts 관련 API
+
+  Future<void> shortsLikeUp(String userId, String shortsId) async {
+    const url_withfunc=url+'shorts/likeUp';
+    final response = await http.post(
+      Uri.parse(url_withfunc),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'userId': userId,
+        'shortsId': shortsId,
+      }),
+    );
+    if (response.statusCode == 200) {
+      print('좋아요 완료');
+    } else {
+      print('좋아요 실패 : ${response.statusCode}');
+    }
+  }
+
+  Future<void> shortsLikeDown(String userId, String shortsId) async {
+    const url_withfunc=url+'shorts/likeDown';
+    final response = await http.post(
+      Uri.parse(url_withfunc),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'userId': userId,
+        'shortsId': shortsId,
+      }),
+    );
+    if (response.statusCode == 200) {
+      print('좋아요 취소 완료');
+    } else {
+      print('좋아요 취소 실패 : ${response.statusCode}');
+    }
+  }
+
+  Future<List<Shorts>> getShortsList() async {
+    const url_withfunc = url + 'shorts/getList';
+    final response = await http.get(
+      Uri.parse(url_withfunc),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+    );
+    if (response.statusCode == 200) {
+      // 응답 처리
+      final responseBody = response.body;
+      final jsonList = jsonDecode(responseBody) as List<dynamic>;
+      final shortsList = jsonList.map((item) => Shorts.fromJson(item)).toList();
+      return shortsList;
+    } else {
+      print('Failed to fetch Shorts List: ${response.statusCode}');
+      throw Exception('Failed to fetch Shorts List: ${response.statusCode}');
+    }
+  }
+
+  // Comment 관련 API
+
+  Future<List<Comment>> getCommentsList(String shortsId) async {
+    const url_withfunc = url + 'comment/getComments';
+    final response = await http.post(
+      Uri.parse(url_withfunc),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'shortsId': shortsId,
+      }),
+    );
+    if (response.statusCode == 200) {
+      // 응답 처리
+      final responseBody = response.body;
+      final jsonList = jsonDecode(responseBody) as List<dynamic>;
+      final commentsList = jsonList.map((item) => Comment.fromJson(item)).toList();
+      return commentsList;
+    } else {
+      print('Failed to fetch Shorts List: ${response.statusCode}');
+      throw Exception('Failed to fetch Shorts List: ${response.statusCode}');
+    }
+  }
 
 
+  Future<void> commentModify(String userId, String commentId, String comment) async {
+    const url_withfunc=url+'comment/modifyComments';
+    final response = await http.post(
+      Uri.parse(url_withfunc),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'userId': userId,
+        'commentId': commentId,
+        'comment' : comment,
+      }),
+    );
+    if (response.statusCode == 200) {
+      print('좋아요 취소 완료');
+    } else {
+      print('좋아요 취소 실패 : ${response.statusCode}');
+    }
+  }
 
 
 }
